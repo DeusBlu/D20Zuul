221a7d7417ed851e5c23c0fb904faa8c5333d804
DeusBlu <DeusBlu@Gmail.com>
2014-02-05 15:21:10 -0800

    cleaned up Item hierarchy and added Consumable as a sub to MagicDevice

---
+++ D20Zuul 0.1_8/src/Consumable.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+
+/**
+ * @author desmond.jenkins
+ *
+ */
+public class Consumable extends MagicDevice {
+	private String[] EFFECT = {
+			"damage", "healing", "buff", "misc"
+	};
+	private String effect;
+	private Damage effectValue;
+	private String type;
+	
+	/**
+	 * default constructor for consumable items
+	 */
+	public Consumable() 
+	{
+		super();
+		setEffect("misc");
+		setEffectValue(new Damage());
+		setType();
+	}
+
+	/**
+	 * @param weight
+	 * @param value
+	 * @param name
+	 * @param charges
+	 * @param effect
+	 * @param effectValue
+	 */
+	public Consumable(double weight, int value, String name, int charges, String effect, Damage effectValue) 
+	{
+		super(weight, value, name, charges);
+		setEffect(effect);
+		setEffectValue(effectValue);
+		setType();
+	}
+	
+	/**
+	 * sets the effect value, a damage object to hold dice and modifiers
+	 * @param effectValue
+	 */
+	private void setEffectValue(Damage effectValue)
+	{
+		if(effectValue != null){
+			this.effectValue = effectValue;
+		}
+		else{
+			throw new IllegalArgumentException("Damage item of " + getName() + " was null");
+		}
+	}
+	
+	/**
+	 * sets the effect type of the item, throws exception if the type is not valid
+	 * @param effect
+	 */
+	private void setEffect(String effect)
+	{
+		for(int i = 0; i < EFFECT.length; i++){
+			if(effect.equalsIgnoreCase(EFFECT[i]))
+				this.effect = effect;
+		}
+		if(this.effect == null){
+			throw new IllegalArgumentException("Effect of item " + getName() + " is not a valid effect type");
+		}
+	}
+	
+	/**
+	 * sets the item type
+	 */
+	private void setType()
+	{
+		type = "Consumable";
+	}
+	
+	/**
+	 * returns the effect value as an int result of the roll
+	 * @return int
+	 */
+	public int getEffectValue()
+	{
+		return effectValue.getDamage();
+	}
+	
+	/**
+	 * returns the effect of the item as a string
+	 * @return String effect
+	 */
+	public String getEffect()
+	{
+		return effect;
+	}
+	
+	/**
+	 * returns the type of item
+	 * @return String type
+	 */
+	public String getType()
+	{
+		return type;
+	}
+}
